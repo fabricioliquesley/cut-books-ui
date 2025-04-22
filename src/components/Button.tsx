772def0065b3f4827d2b5d9f908769e38fa95533
Fabricio Liqueley <fabricioliquesley15@gmail.com>
2025-04-22 10:28:18 -0300

    adapts the Button and DropArea components to have a more flexible style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,19 @@
+import { cn } from "@/lib/utils";
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
+  className?: string;
 }
 
-export function Button({ children, ...props }: ButtonProps) {
+export function Button({ children, className, ...props }: ButtonProps) {
   return (
     <button
       {...props}
-      className="w-full cursor-pointer rounded-lg bg-white p-2 font-bold text-sky-800 brightness-75 transition-all hover:brightness-100"
+      className={cn(
+        "w-full cursor-pointer rounded-lg bg-white p-2 font-bold text-sky-800 brightness-75 transition-all hover:brightness-100",
+        className,
+      )}
     >
       {children}
     </button>
--- src/components/DropArea.tsx
+++ src/components/DropArea.tsx
@@ -1,10 +1,12 @@
 import { cn } from "@/lib/utils";
 import { FilePlus2 } from "lucide-react";
+import { InputHTMLAttributes } from "react";
 
 type VariantType = "primary" | "icon";
 
-interface DropAreaProps {
+interface DropAreaProps extends InputHTMLAttributes<HTMLInputElement> {
   variant?: VariantType;
+  className?: string;
 }
 
 const variants: Record<VariantType, string> = {
@@ -12,19 +14,25 @@ const variants: Record<VariantType, string> = {
   icon: "h-14 w-14 rounded-full",
 };
 
-export function DropArea({ variant = "primary" }: DropAreaProps) {
+export function DropArea({
+  variant = "primary",
+  className,
+  ...props
+}: DropAreaProps) {
   return (
     <div
       className={cn(
         "relative flex items-center justify-center border-2 border-neutral-500 bg-neutral-400 transition-all hover:ring-4 hover:ring-white/20",
         variants[variant],
+        className,
       )}
     >
       <FilePlus2 size={32} />
-      {variant === "primary" && <p>Select your file</p>}
+      {variant !== "icon" && <p>Select your file</p>}
       <input
         type="file"
         className="absolute h-full w-full bg-red-500 opacity-0"
+        {...props}
       />
     </div>
   );
