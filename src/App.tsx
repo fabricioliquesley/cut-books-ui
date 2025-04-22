e2eae5be2eb6a7195de61769e8331062cfe50454
Fabricio Liqueley <fabricioliquesley15@gmail.com>
2025-04-22 10:29:15 -0300

    finish the structure and styles of home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 import { BookFileTree } from "./components/BookFileTree";
+import { ChangeEvent, useState } from "react";
+import { DropArea } from "./components/DropArea";
+import { toast } from "sonner";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./components/ui/tooltip";
 
 const ELEMENTS = [
   {
@@ -91,16 +100,63 @@ const ELEMENTS = [
 ];
 
 export function App() {
+  const [elements, setElements] = useState([
+    {
+      id: "1",
+      isSelectable: true,
+      name: "books",
+      children: [],
+    },
+  ]);
+  const [isSelectedFile, setIsSelectedFile] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files;
+
+    if (!selectedFile) {
+      return toast.warning("Select a file");
+    }
+
+    if (selectedFile[0].type !== "application/pdf") {
+      return toast.error("Only pdf files can be selected");
+    }
+
+    setFile(selectedFile[0]);
+    setIsSelectedFile(true);
+  };
+
+  if (!isSelectedFile) {
+    return <DropArea onChange={handleFileChange} />;
+  }
+
   return (
-    <div className="grid max-w-[720px] grid-cols-3 items-center gap-8">
+    <div className="grid max-w-[800px] grid-cols-4 items-center gap-8">
       <form className="col-span-2 space-y-4">
-        <Input placeholder="Filename" />
         <Input placeholder="Book" />
         <Input placeholder="Range" />
         <Button>Add chapter</Button>
       </form>
-      <div>
-        <BookFileTree elements={ELEMENTS} className="col-span-1" />
+      <div className="relative col-span-2">
+        <h2 className="text-center text-2xl text-sky-600">{file?.name}</h2>
+        <BookFileTree elements={ELEMENTS} />
+        <Button className="hover: absolute bottom-2 ml-[2.5%] w-[95%] bg-sky-800 text-neutral-100">
+          Cut Book
+        </Button>
+      </div>
+      <div className="absolute right-10 bottom-10">
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <DropArea onChange={handleFileChange} variant="icon" />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p className="rounded-lg bg-neutral-700 p-2">
+                Change selected file
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { Toaster } from "sonner";
+
 import { App } from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <div className="flex h-screen w-full items-center justify-center bg-neutral-900 p-4">
+      <Toaster position="top-center" richColors />
       <App />
     </div>
   </StrictMode>,
